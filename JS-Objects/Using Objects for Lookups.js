0336878f7eb37d492fecb9456b2d0928835fbd88
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-06 23:30:09 -0500

    Create Using Objects for Lookups.js

---
+++ JS-Objects/Using Objects for Lookups.js
@@ -0,0 +1,41 @@
+/*
+Objects can be thought of as a key/value storage, like a dictionary. If you have tabular data, you can use an object to lookup values rather than a switch statement or an if/else chain. This is most useful when you know that your input data is limited to a certain range.
+
+Here is an example of an article object:
+
+const article = {
+  "title": "How to create objects in JavaScript",
+  "link": "https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/",
+  "author": "Kaashan Hussain",
+  "language": "JavaScript",
+  "tags": "TECHNOLOGY",
+  "createdAt": "NOVEMBER 28, 2018"
+};
+
+const articleAuthor = article["author"];
+const articleLink = article["link"];
+
+const value = "title";
+const valueLookup = article[value];
+*/
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  const lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+
+  result = lookup[val];
+  console.log(result);
+
+  // Only change code above this line
+  return result;
+}
+phoneticLookup("charlie");
